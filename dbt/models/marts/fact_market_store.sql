1f145c8748f5f148d7d2e78213da6979456c2cf0
yewon918 <yewon918@naver.com>
2025-12-22 16:39:03 +0900

    feat: market store dbt 실행 추가

---
+++ dbt/models/marts/fact_market_store.sql
@@ -0,0 +1,16 @@
+WITH intermediate_data AS (
+    SELECT * FROM {{ref('int_market_ppl')}}
+)
+
+SELECT
+    area_name,
+    major_category,
+    medium_category,
+    busyness,
+    payment_count,
+    payment_min,
+    payment_max,
+    merchant_count
+    loaded_at
+
+FROM intermediate_data
