669e22e214540f2494dca7ca6713a6080e0da75b
hanbyeolkang <strongstar1210@gmail.com>
2025-12-22 10:57:16 +0900

    refactor: 강우량 dbt 개선

--- dbt/models/intermediate/int_rainfall.sql
+++ dbt/models/intermediate/int_rainfall.sql
@@ -1,11 +1,11 @@
 {{
     config(
         materialized='incremental',
-        unique_key='rf_cd',
+        unique_key='gu_cd',
         on_schema_change='append_new_columns'
     )
 }}
-SELECT DISTINCT ON (rf_cd)
+SELECT DISTINCT ON (gu_cd)
     rf_cd,
     rf_nm,
     gu_cd,
@@ -15,6 +15,10 @@ SELECT DISTINCT ON (rf_cd)
     updated_at
 FROM {{ ref('stg_list_rainfall_service') }}
 {% if is_incremental() %}
-WHERE updated_at > (SELECT max(updated_at) FROM {{ this }})
+WHERE data_clct_tm >= (SELECT MAX(data_clct_tm) - INTERVAL '1 hours' FROM {{ this }})
 {% endif %}
-ORDER BY rf_cd, data_clct_tm DESC, updated_at DESC
+ORDER BY 
+    gu_cd,
+    data_clct_tm DESC,  -- 1순위: 각 구별로 가장 최신 시간 데이터를 선택
+    rn_10m DESC,        -- 2순위: 같은 시간이라면 강우량이 높은 것 선택
+    rf_cd ASC           -- 3순위: 나머지 경우 코드 순
