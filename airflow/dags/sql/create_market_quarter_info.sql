b8e8dbc95a9c4865ea22e3f5a0a1da1f8421806a
yewon918 <yewon918@naver.com>
2026-01-05 15:15:25 +0900

    fix: 괄호 수정

--- airflow/dags/sql/create_market_quarter_info.sql
+++ airflow/dags/sql/create_market_quarter_info.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS "raw_data";
 
 CREATE TABLE IF NOT EXISTS "raw_data"."3Q_market_info" (
-    {{ create_columns_with_types }}
+    { create_columns_with_types }
 );
 
 TRUNCATE TABLE "raw_data"."3Q_market_info";
